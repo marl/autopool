729da7a8b093549b68a44feaadec92ccae72612e
Brian McFee <brian.mcfee@nyu.edu>
2018-04-24 11:51:48 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+import imp
+
+version = imp.load_source('autopool.version', 'autopool/version.py')
+description='Adaptive pooling operators for multiple instance learning'
+
+setup(
+    name='autopool',
+    version=version.version,
+    description=description,
+    author='MARL',
+    author_email='brian.mcfee@nyu.edu',
+    url='http://github.com/marl/autopool',
+    download_url='http://github.com/marl/autopool/releases',
+    packages=find_packages(),
+    long_description=description,
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='deep learning, multiple instance learning, machine learning',
+    license='MIT',
+    install_requires=[
+        'keras>=2.0',
+    ],
+    extras_require={ 'docs': ['numpydoc', 'sphinx-gallery'] }
+)
