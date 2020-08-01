d32f630e1dedffbf92b55748fbeb08eda3e2339f
yuvaram singh <yuvaramsingh94@gmail.com>
2020-08-01 21:15:01 +0530

    Add files via upload

---
+++ pytorch/autopool.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+'''
+This implimentation is tested for multi label network
+'''
+
+class Autopool(nn.Module):
+
+
+    '''
+    input_size = no of output labels
+    '''
+    def __init__(self, input_size ):
+    
+        super(Autopool, self).__init__()
+        self.alpha = torch.ones([input_size], dtype=torch.float64, requires_grad= True)
+        self.alpha = self.alpha.to(device)
+        self.sigmoid_layer = nn.Sigmoid()
+        self.softmax_layer = nn.Softmax(dim=2)
+        
+        
+    def forward(self,x):
+        sigmoid_output = self.sigmoid_layer(x)
+        alpa_mult_out = torch.mul(sigmoid_output, self.alpha)
+        weights = self.softmax_layer(alpa_mult_out)
+        weighted_output = torch.mul(sigmoid_output, weights)
+        final_out = torch.sum(weighted_output, dim=1)
+        return final_out
